7f75cd587ebd10b4a0c653bc742bfda42de68172
Wadim Woytenko <woytenko.w@yandex.ru>
2020-03-26 20:48:11 +0300

    LABS 1-2 RELEASE

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,6 @@ angular.module('designApp', [
   'ngResource'
 ]).config(function ($mdThemingProvider) {
   $mdThemingProvider.theme('default')
-    .primaryPalette('deep-purple')
+    .primaryPalette('green')
     .accentPalette('teal');
 });
